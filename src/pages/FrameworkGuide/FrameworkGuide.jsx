11b3e86b66ec7379ad3197e9a17be7b88213da8a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-07-20 17:24:07 +0530

    Update FrameworkGuide.jsx

--- src/pages/FrameworkGuide/FrameworkGuide.jsx
+++ src/pages/FrameworkGuide/FrameworkGuide.jsx
@@ -4,6 +4,10 @@ const FrameworkGuide = () => {
     return (
         <div>
             <h1 className="text-2xl tracking-[5px] font-semibold text-gray-500 uppercase">Framework Guide</h1>
+            <p className="mt-4 text-base text-gray-600 leading-relaxed">
+                After JKCSS v5 work as Vite Plugin, so now use with any any framework or library (ReactJS etc) 
+            </p>
+
         </div>
     )
 }
